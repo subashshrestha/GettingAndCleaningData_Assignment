52ebc3c78ac7776a916a07fcb51e478badbf58fd
Subash Shrestha <info@subashstha.com.np>
2016-04-20 15:52:42 +0545

    GettingAndCleaningData Assignment Completed.

---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+## By Subash Shrestha for Coursera: Getting and Cleaning Data (Assignment) 2016 April 20
+
+## Create one R script called run_analysis.R that does the following:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive activity names.
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+run_analysis <- function(){
+  # load test data  
+  subject_test = read.table("UCI HAR Dataset/test/subject_test.txt")
+  X_test = read.table("UCI HAR Dataset/test/X_test.txt")
+  Y_test = read.table("UCI HAR Dataset/test/Y_test.txt")
+  
+  # load training data
+  subject_train = read.table("UCI HAR Dataset/train/subject_train.txt")
+  X_train = read.table("UCI HAR Dataset/train/X_train.txt")
+  Y_train = read.table("UCI HAR Dataset/train/Y_train.txt")
+  
+  # load lookup information
+  features <- read.table("UCI HAR Dataset/features.txt", col.names=c("featureId", "featureLabel"))
+  activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("activityId", "activityLabel"))
+  activities$activityLabel <- gsub("_", "", as.character(activities$activityLabel))
+  includedFeatures <- grep("-mean\\(\\)|-std\\(\\)", features$featureLabel)
+  
+  # merge test and training data and then name them
+  subject <- rbind(subject_test, subject_train)
+  names(subject) <- "subjectId"
+  X <- rbind(X_test, X_train)
+  X <- X[, includedFeatures]
+  names(X) <- gsub("\\(|\\)", "", features$featureLabel[includedFeatures])
+  Y <- rbind(Y_test, Y_train)
+  names(Y) = "activityId"
+  activity <- merge(Y, activities, by="activityId")$activityLabel
+  
+  # merge data frames of different columns to form one data table
+  data <- cbind(subject, X, activity)
+  write.table(data, "merged_tidy_data.txt")
+  
+  # create a dataset grouped by subject and activity after applying standard deviation and average calculations
+  library(data.table)
+  dataDT <- data.table(data)
+  calculatedData<- dataDT[, lapply(.SD, mean), by=c("subjectId", "activity")]
+  write.table(calculatedData, "tidy_cal_data.txt")
+}
